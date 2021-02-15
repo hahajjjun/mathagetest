6aba1d5fb28e9a27634ec6c5fb2c5b29077b66c3
haha <haha@haha.kr>
2021-02-15 14:56:06 +0900

    first commit

---
+++ js/share.js
@@ -0,0 +1,24 @@
+const url = 'https://hahajjjun.github.io/mathagetest/.';
+const title = '수학 사춘기 테스트';
+const hash = '%23math adolescence test %23수학사춘기테스트';
+
+const fb = () => {
+  window.open('https://www.facebook.com/sharer/sharer.php?u='
+    + url + '&t=' + title + '' + hash,
+    'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+  return false;
+}
+
+const nv = () => {
+  window.open('http://share.naver.com/web/shareView.nhn?url='
+    + url + '&title=' + title,
+    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+  return false;
+}
+
+const band = () => {
+  window.open('https://band.us/plugin/share?url='
+    + url + '&title=' + title,
+    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+  return false;
+}
